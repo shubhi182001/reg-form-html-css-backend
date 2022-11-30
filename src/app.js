371cd6029a428629fb8e7bed118345d19aee427c
shubhi182001 <kediashubhi@gmail.com>
2022-12-01 02:44:14 +0530

    set up of template engine done

--- src/app.js
+++ src/app.js
@@ -2,19 +2,24 @@ const express = require("express");
 require("./db/conn");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.use(express.json()); 
 
 //hbs part:
 console.log(path.join(__dirname , "../public"));
 const static_path = path.join(__dirname, "../public");
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 app.use(express.static(static_path));
-
-
+app.set("view engine", "hbs");
+app.set("views", templates_path);
+hbs.registerPartials(partials_path)
 app.get("/", (req, res) => {
-    res.send("hello from shubhi");
+    res.render("index");
 });
+
 
 app.listen(port, () =>{
     console.log(`server is running at ${port}`);
