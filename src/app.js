51687815890a91a033a4896978f72b205bb834f3
shubhi182001 <kediashubhi@gmail.com>
2022-12-03 10:45:29 +0530

    dotenv

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log(process.env.DATABASE_NAME)
 const express = require("express");
 require("./db/conn");
 const path = require("path");
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/registerform").then(() => {
+mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE_NAME}`).then(() => {
     console.log("connected to database");
 }).catch((e) => {
     console.log(e);
